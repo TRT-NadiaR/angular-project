1bd56e4a82c05a912f864cdfc8fde572eb908304
TRT-NadiaR <nadia.rashad@raytheon.co.uk>
2020-10-23 09:27:42 +0100

    added form submit which saves data to local storage, also an edit details section once submitted, need to fix age as this is not being saved when using the drop down selection

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -11,19 +11,29 @@ export class RegisterComponent implements OnInit {
 
   ageOfChild = ["select age", '9 months','10 months','11 months', '1', '2', '3', '4'];
   model = new Child (18, '', "",this.ageOfChild[0], '', '');
+
   submitted = false;
   
   onSubmit() { 
     this.submitted = true; 
+    console.log("model", this.model)
+    localStorage.setItem('testObject', JSON.stringify(this.model));
+
+    // let retrievedObject = localStorage.getItem('this.model');
+    // console.log('retrievedObject: ', JSON.parse(retrievedObject));
   }
 
   newChild() {
     this.model = new Child(10, '', '', '', '', '');
+
   }
 
   get diagnostic() { 
     return JSON.stringify(this.model);
+   
   }
+
+  
 
   constructor() { }
 
